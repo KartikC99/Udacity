46f563c45ef36da509fcc9a01d96b07a9d3b426b
Kartik <50834578+KartikC99@users.noreply.github.com>
2022-07-22 15:23:40 +1000

    Add files via upload
    Project1 full directory

---
+++ Project1/Project/script/myscript.cpp
@@ -0,0 +1,13 @@
+#include<gazebo/gazebo.hh>
+namespace gazebo
+{
+    class WorldPluginProject:public WorldPlugin{
+        public:WorldPluginProject():WorldPlugin(){
+            printf("Welcome to Kartik \'s World !\n");
+        }
+        public: void Load(physics::WorldPtr _world,sdf::ElementPtr _sdf){
+
+        }
+    };
+    GZ_REGISTER_WORLD_PLUGIN(WorldPluginProject)
+}
